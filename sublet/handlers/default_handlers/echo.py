5235b1f21db9228cb359a5d52c3b584a878d72ae
alexeyglinkin <aleksey.glinkin@skillbox.ru>
2024-05-10 14:17:46 +0300

    исправление бд9

--- sublet/handlers/default_handlers/echo.py
+++ sublet/handlers/default_handlers/echo.py
@@ -16,8 +16,8 @@ def bot_echo(message: Message) -> None:
         try:
             all_us = ", ".join([i for i in all_users])
             bot.send_message(message.from_user.id, all_us)
-        except:
-            bot.send_message(message.from_user.id, 'Что-то не так')
+        except Exception as e:
+            bot.send_message(message.from_user.id, str(e))
 
     else:
         logger.warning(f'{message.from_user.username} — ECHO — {message.text}')
