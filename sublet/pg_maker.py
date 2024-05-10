3bfaa859c45e16c0317a2043ec2e84a2265f4db6
alexeyglinkin <aleksey.glinkin@skillbox.ru>
2024-05-10 14:15:35 +0300

    исправление бд8

--- sublet/pg_maker.py
+++ sublet/pg_maker.py
@@ -65,8 +65,8 @@ def add_user(username):
     conn, cursor = connect_to_db()
     create_users()
     sql = """INSERT INTO public.users 
-    username
-    VALUES (%s);
+(username)
+VALUES (%s);
     """
     cursor.execute(sql, (username,))
     close_db_connection(conn, cursor)
