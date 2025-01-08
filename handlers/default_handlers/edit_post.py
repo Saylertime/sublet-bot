b63e73a25d5405e3204a456e1fe0b68f065cdea2
alexeyglinkin <aleksey.glinkin@skillbox.ru>
2025-01-08 13:40:43 +0300

    fix3

--- handlers/default_handlers/edit_post.py
+++ handlers/default_handlers/edit_post.py
@@ -182,6 +182,11 @@ def see_post(message):
         markup = create_markup(buttons)
         bot.send_message(message.from_user.id, 'Что дальше?', reply_markup=markup)
     except Exception as e:
+        media.append(InputMediaPhoto(open(user_photos[0], 'rb').read()))
+        for photo_path in user_photos[1:]:
+            with open(photo_path, 'rb') as photo_file:
+                media.append(InputMediaPhoto(photo_file.read()))
+        bot.send_media_group(message.from_user.id, media)
         # bot.send_message(message.from_user.id, "Загружены некорректные фото. Они должны быть в формате jpeg или png")
         bot.send_message(message.from_user.id, msg)
         try:
