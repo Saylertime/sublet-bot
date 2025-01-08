dcda0fc049c548e838a2f084fbd400bcb7393fb2
alexeyglinkin <aleksey.glinkin@skillbox.ru>
2025-01-08 13:47:13 +0300

    fix6

--- handlers/default_handlers/edit_post.py
+++ handlers/default_handlers/edit_post.py
@@ -182,12 +182,12 @@ def see_post(message):
         markup = create_markup(buttons)
         bot.send_message(message.from_user.id, 'Что дальше?', reply_markup=markup)
     except Exception as e:
-        media.append(InputMediaPhoto(open(user_photos[0], 'rb').read()))
+        media.append(InputMediaPhoto(open(user_photos[0], 'rb').read(), caption=None))
         for photo_path in user_photos[1:]:
             with open(photo_path, 'rb') as photo_file:
                 media.append(InputMediaPhoto(photo_file.read()))
         try:
-            bot.send_media_group(message.from_user.id, media, caption=None)
+            bot.send_media_group(message.from_user.id, media)
         except Exception as e:
             bot.send_message(message.from_user.id, e)
         bot.send_message(message.from_user.id, msg)
